def54c8f02c9b05ead92ec73070a166faf571c7b
Ahmed Fayad <afayad323@gmail.com>
2026-05-06 12:08:25 +0300

    refactor: improve error handling, unify responses, add query pagination and enable CORS

--- controllers/courses.controllers.js
+++ controllers/courses.controllers.js
@@ -1,13 +1,23 @@
 const Course = require("../models/models.courses");
 const { validationResult } = require("express-validator");
+const httpStatusText = require("../utils/httpstatusText");
 
 const getAllCourses = async (req, res) => {
   try {
-    const courses = await Course.find();
+    const query = req.query;
+    const limit = query.limit || 10;
+    const page = query.page || 1;
+    const skip = (page - 1) * limit;
+    const courses = await Course.find().limit(limit).skip(skip);
 
-    res.status(200).json(courses);
+    res.status(200).json({ status: httpStatusText.SUCCESS, data: { courses } });
   } catch (err) {
-    return res.status(400).json(err);
+    return res.status(500).json({
+      status: httpStatusText.ERROR,
+      data: null,
+      message: err.message,
+      code: 500,
+    });
   }
 };
 
@@ -15,10 +25,19 @@ const getCourseById = async (req, res) => {
   try {
     let course = await Course.findById(req.params.id);
 
-    if (!course) return res.status(404).json({ message: "Course not found" });
-    res.status(200).json(course);
+    if (!course)
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { course: "course not found" },
+      });
+    res.status(200).json({ status: httpStatusText.SUCCESS, data: { course } });
   } catch (err) {
-    return res.status(400).json({ msg: "invalid id" });
+    return res.status(500).json({
+      status: httpStatusText.ERROR,
+      data: null,
+      message: err.message,
+      code: 500,
+    });
   }
 };
 
@@ -26,13 +45,23 @@ const createCourse = async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({
+        status: httpStatusText.FAIL,
+        data: { errors: errors.array() },
+      });
     }
     const newCourse = await Course.create(req.body);
 
-    res.status(201).json(newCourse);
+    res
+      .status(201)
+      .json({ status: httpStatusText.SUCCESS, data: { newCourse } });
   } catch (err) {
-    return res.status(400).json(err);
+    return res.status(500).json({
+      status: httpStatusText.ERROR,
+      data: null,
+      message: err.message,
+      code: 500,
+    });
   }
 };
 
@@ -40,19 +69,30 @@ const updateCourse = async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({
+        status: httpStatusText.FAIL,
+        data: { errors: errors.array() },
+      });
     }
 
     const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
     if (!course) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { course: "course not found" },
+      });
     }
 
-    res.status(200).json(course);
+    res.status(200).json({ status: httpStatusText.SUCCESS, data: { course } });
   } catch (err) {
-    return res.status(400).json({ msg: "invalid id" });
+    return res.status(500).json({
+      status: httpStatusText.ERROR,
+      data: null,
+      message: err.message,
+      code: 500,
+    });
   }
 };
 
@@ -60,11 +100,19 @@ const deleteCourse = async (req, res) => {
   try {
     let course = await Course.findByIdAndDelete(req.params.id);
     if (!course) {
-      return res.status(404).json({ message: "Course not found" });
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { course: "course not found" },
+      });
     }
-    res.status(200).json({ message: "Course deleted" });
+    res.status(200).json({ status: httpStatusText.SUCCESS, data: null });
   } catch (err) {
-    return res.status(400).json({ msg: "invalid id" });
+    return res.status(500).json({
+      status: httpStatusText.ERROR,
+      data: null,
+      message: err.message,
+      code: 500,
+    });
   }
 };
 
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const cors = require("cors");
 require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
+const httpStatusText = require("./utils/httpstatusText");
 
 console.log("ENV:", process.env.MONGO_URL);
 console.log(process.env.MONGO_URL);
@@ -10,12 +13,18 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("mongodb connected success");
 });
 app.use(express.json());
+app.use(cors());
 const port = 3000;
 
 const courseRouter = require("./routes/courses.routes");
 
 app.use("/api/courses", courseRouter);
-
+app.use((req, res) => {
+  res.status(404).json({
+    status: httpStatusText.ERROR,
+    message: "this resource isn't available",
+  });
+});
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
